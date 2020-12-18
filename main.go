6175fbf18b4fa61385a63232ecaa45f06f4cbe08
Alvin <alvinjasm@gmail.com>
2020-12-17 22:51:56 -0800

    send get request to httpbin

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
+// GET /geolocate/<ip_address> IP geolocation that queries an IP address and returns location data, using a free service such as ipgeolocation.io
+// GET /swagger/index.html an endpoint that displays Swagger/OpenAPI specification of the endpoint you created above. We recommend using echo-swagger for this.
+// Finally, once you've generated your OpenAPI specification you will use Swagger codegen to generate a Golang SDK so that other Go applications can use and access your API. Add the client code in a folder of your submission named /client.
+
 package main
 
 import (
   "net/http"
+  "io/ioutil"
   "github.com/labstack/echo/v4"
   "github.com/labstack/echo/v4/middleware"
 )
@@ -16,6 +21,7 @@ func main() {
 
   // Routes
   e.GET("/", hello)
+  e.GET("/geolocate", geolocate)
 
   // Start server
   e.Logger.Fatal(e.Start(":1323"))
@@ -24,4 +30,17 @@ func main() {
 // Handler
 func hello(c echo.Context) error {
   return c.String(http.StatusOK, "Hello, World!")
+}
+
+func geolocate(c echo.Context) error {
+  resp, err := http.Get("https://httpbin.org/get")
+  if err != nil {
+    // handle error
+  }
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    // handle error
+  }
+  return c.String(http.StatusOK, string(body))
 }
